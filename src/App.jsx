bda45ffa5154076cdbdca3f7af061e1b93396437
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-04-05 13:12:03 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import Heading from './components/Heading/Heading';
+import { lazy, Suspense } from 'react';
+
+import Loader from './components/Loader/Loader';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+const Home = () => lazy(() => import('./pages/Home'));
+
+const Rates = () => lazy(() => import('./pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 };
